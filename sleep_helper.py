1755c8adade59365ad315c314e5b24d6340c4baa
Ruslan Lukinov <elvisstudio@mail.ru>
2024-11-11 09:06:56 +0300

    v2
    Склонения

--- sleep_helper.py
+++ sleep_helper.py
@@ -1,8 +1,16 @@
 # Запрашиваем у пользователя количество овец
 num_sheep = int(input("Введите количество овец: "))
 
+def sheep_plural(n):
+    if n % 10 == 1 and n % 100 != 11:
+        return "овца"
+    elif n % 10 in [2, 3, 4] and n % 100 not in [12, 13, 14]:
+        return "овцы"
+    else:
+        return "овец"
+
 # Считаем овец
 for i in range(1, num_sheep + 1):
-    print(f"{i} овца...")
+    print(f"{i} {sheep_plural(i)} за забором")
 
 print("Все овцы посчитаны!")
